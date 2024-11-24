5fc9534d18447259c2bacc3cb16f5c293c7f3c0e
Sarabella <110487056+BellaAkins@users.noreply.github.com>
2024-11-23 17:41:12 -0800

    Create products.js

---
+++ data/products.js
@@ -0,0 +1,103 @@
+export const products = [
+  {
+    id: "1",
+    image: "images/products/men-chino-pants-beige.jpg",
+    name: "Men's Classic-fit Pleated Chino Pants",
+    rating: {
+      stars: 4.5,
+      count: 9017,
+    },
+    priceCents: 2290,
+    keywords: ["pants", "apparel", "mens"],
+  },
+
+  {
+    id: "2",
+    image: "images/products/men-cozy-fleece-zip-up-hoodie-red.jpg",
+    name: "Men's Full-Zip Hooded Fleece Sweatshirt",
+    rating: {
+      stars: 4.5,
+      count: 3157,
+    },
+    priceCents: 2400,
+    keywords: ["sweaters", "hoodies", "apparel", "mens"],
+  },
+
+  {
+    id: "3",
+    image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
+    name: "Adults Plain Cotton T-Shirt - 2 Pack",
+    rating: {
+      stars: 4.5,
+      count: 56,
+    },
+    priceCents: 799,
+    keywords: ["tshirts", "apparel", "mens"],
+    type: "clothing",
+    sizeChartLink: "images/clothing-size-chart.png",
+  },
+
+  {
+    id: "4",
+    image: "images/products/plain-hooded-fleece-sweatshirt-yellow.jpg",
+    name: "Plain Hooded Fleece Sweatshirt",
+    rating: {
+      stars: 4.5,
+      count: 317,
+    },
+    priceCents: 2400,
+    keywords: ["hoodies", "sweaters", "apparel"],
+  },
+
+  {
+    id: "5",
+    image: "images/products/women-chiffon-beachwear-coverup-black.jpg",
+    name: "Women's Chiffon Beachwear Cover Up - Black",
+    rating: {
+      stars: 4.5,
+      count: 235,
+    },
+    priceCents: 2070,
+    keywords: ["robe", "swimsuit", "swimming", "bathing", "apparel"],
+    type: "clothing",
+    sizeChartLink: "images/clothing-size-chart.png",
+  },
+
+  {
+    id: "6",
+    image: "images/products/men-slim-fit-summer-shorts-gray.jpg",
+    name: "Men's Slim-Fit Summer Shorts",
+    rating: {
+      stars: 4,
+      count: 160,
+    },
+    priceCents: 1699,
+    keywords: ["shorts", "apparel", "mens"],
+  },
+
+  {
+    id: "7",
+    image: "images/products/women-stretch-popover-hoodie-black.jpg",
+    name: "Women's Stretch Popover Hoodie",
+    rating: {
+      stars: 4.5,
+      count: 2465,
+    },
+    priceCents: 1374,
+    keywords: ["hooded", "hoodies", "sweaters", "womens", "apparel"],
+    type: "clothing",
+    sizeChartLink: "images/clothing-size-chart.png",
+  },
+
+  {
+    id: "8",
+    image: "images/products/women-french-terry-fleece-jogger-camo.jpg",
+    name: "Women's Fleece Jogger Sweatpant",
+    rating: {
+      stars: 4.5,
+      count: 248,
+    },
+    priceCents: 2400,
+    keywords: ["pants", "sweatpants", "jogging", "apparel", "womens"],
+  },
+];

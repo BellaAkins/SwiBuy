f4061102eb6e754aed19c1e2d2113ed671dcd9c6
Sarabella <110487056+BellaAkins@users.noreply.github.com>
2024-11-27 15:35:30 -0800

    Update products.js

--- data/products.js
+++ data/products.js
@@ -1,18 +1,29 @@
+export function getProduct(productId) {
+  let matchingProduct;
+
+  products.forEach((product) => {
+    if (product.id === productId) {
+      matchingProduct = product;
+    }
+  });
+
+  return matchingProduct;
+}
+
 export const products = [
   {
-    id: "1",
+    id: "36c64692-677f-4f58-b5ec-0dc2cf109e27",
     image: "images/products/men-chino-pants-beige.jpg",
     name: "Men's Classic-fit Pleated Chino Pants",
     rating: {
-      stars: 4.5,
-      count: 9017,
+      stars: 5,
+      count: 679,
     },
-    priceCents: 2290,
+    priceCents: 3899,
     keywords: ["pants", "apparel", "mens"],
   },
-
   {
-    id: "2",
+    id: "aaa65ef3-8d6f-4eb3-bc9b-a6ea49047d8f",
     image: "images/products/men-cozy-fleece-zip-up-hoodie-red.jpg",
     name: "Men's Full-Zip Hooded Fleece Sweatshirt",
     rating: {
@@ -22,9 +33,8 @@ export const products = [
     priceCents: 2400,
     keywords: ["sweaters", "hoodies", "apparel", "mens"],
   },
-
   {
-    id: "3",
+    id: "bc2847e9-5323-403f-b7cf-57fde044a955",
     image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
     name: "Adults Plain Cotton T-Shirt - 2 Pack",
     rating: {
@@ -36,9 +46,8 @@ export const products = [
     type: "clothing",
     sizeChartLink: "images/clothing-size-chart.png",
   },
-
   {
-    id: "4",
+    id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
     image: "images/products/plain-hooded-fleece-sweatshirt-yellow.jpg",
     name: "Plain Hooded Fleece Sweatshirt",
     rating: {
@@ -48,9 +57,8 @@ export const products = [
     priceCents: 2400,
     keywords: ["hoodies", "sweaters", "apparel"],
   },
-
   {
-    id: "5",
+    id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
     image: "images/products/women-chiffon-beachwear-coverup-black.jpg",
     name: "Women's Chiffon Beachwear Cover Up - Black",
     rating: {
@@ -62,9 +70,8 @@ export const products = [
     type: "clothing",
     sizeChartLink: "images/clothing-size-chart.png",
   },
-
   {
-    id: "6",
+    id: "83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
     image: "images/products/men-slim-fit-summer-shorts-gray.jpg",
     name: "Men's Slim-Fit Summer Shorts",
     rating: {
@@ -74,9 +81,8 @@ export const products = [
     priceCents: 1699,
     keywords: ["shorts", "apparel", "mens"],
   },
-
   {
-    id: "7",
+    id: "54e0eccd-8f36-462b-b68a-8182611d9add",
     image: "images/products/women-stretch-popover-hoodie-black.jpg",
     name: "Women's Stretch Popover Hoodie",
     rating: {
@@ -88,9 +94,8 @@ export const products = [
     type: "clothing",
     sizeChartLink: "images/clothing-size-chart.png",
   },
-
   {
-    id: "8",
+    id: "77a845b1-16ed-4eac-bdf9-5b591882113d",
     image: "images/products/women-french-terry-fleece-jogger-camo.jpg",
     name: "Women's Fleece Jogger Sweatpant",
     rating: {

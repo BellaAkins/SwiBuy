7f0014d4c3ec8cac635b9398ad5451df5937bac2
Sarabella <110487056+BellaAkins@users.noreply.github.com>
2024-11-30 01:43:41 -0800

    Update cart.js

--- data/cart.js
+++ data/cart.js
@@ -1,20 +1,23 @@
 export let cart = JSON.parse(localStorage.getItem("cart"));
-/*
+
 if (!cart) {
+  /*
   cart = [
     {
       productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-      quantity: 2,
+      quantity: 0,
       deliveryOptionId: "1",
     },
 
     {
       productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-      quantity: 1,
+      quantity: 0,
       deliveryOptionId: "2",
     },
-  ];
-}*/
+  ];*/
+
+  cart = [];
+}
 
 function saveToStorage() {
   localStorage.setItem("cart", JSON.stringify(cart));
@@ -101,11 +104,8 @@ export function updateDeliveryOption(productId, deliveryOptionId) {
   saveToStorage();
 }
 
-
-/*function clearStorage() {
+export function clearStorage() {
   localStorage.clear();
 }
-
-clearStorage();*/
 
 //CHECK ON KEY AND TRY TO WRITE IT WITH FIND LIKE AI
